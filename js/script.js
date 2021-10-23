13dd3070c56a38f262db38a7c5972d1dda78b742
Léo Genot <45610291+leogenot@users.noreply.github.com>
2021-10-23 15:18:56 +0200

    Portfolio 2021 update
    brand new portfolio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,44 @@
+var scrollSlider = document.getElementById("scroll-range");
+var panel = document.getElementById("page-content");
+
+var total = panel.scrollHeight - panel.offsetHeight;
+var percentage = total * (this.value / 100);
+var percentageHeight = total / 10; //10% for each +/- click
+
+scrollSlider.oninput = function () {
+
+  panel = document.getElementById("page-content");
+  total = panel.scrollHeight - panel.offsetHeight;
+  percentage = total * (this.value / 100);
+  panel.scrollTop = percentage;
+
+}
+
+const slider = document.getElementById("page-content");
+let isDown = false;
+let startY;
+let scrollTop;
+
+panel.addEventListener('scroll', (e) => {
+  total = panel.scrollHeight - panel.offsetHeight;
+  percentage = panel.scrollTop / total
+  scrollSlider.value = percentage * 100
+
+})
+
+
+function openOverlay(element) {
+  document.getElementById(element).style.height = "100%";
+}
+
+function closeOverlay(element) {
+  document.getElementById(element).style.height = "0%";
+}
+
+
 const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
-document.getElementById('js-scroll').appendChild(lightbox)
+document.body.appendChild(lightbox)
 
 const images = document.querySelectorAll('img')
 images.forEach(image => {
@@ -17,5 +55,9 @@ images.forEach(image => {
 
 lightbox.addEventListener('click', e => {
   if (e.target !== e.currentTarget) return
-    lightbox.classList.remove('active')
+  lightbox.classList.remove('active')
 })
+
+
+
+
