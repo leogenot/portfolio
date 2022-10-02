6c1985deac5db7d603b7e3c61f28336c3f4bd595
Léo Genot <lg@mambomambo.ca>
2022-10-02 15:26:40 +0200

    ui

--- src/index.ts
+++ src/index.ts
@@ -4,61 +4,9 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 
-/* const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-});
-
-
-scroll.on("scroll", ScrollTrigger.update);
-
-ScrollTrigger.scrollerProxy('[data-scroll-container]', {
-    scrollTop(value) {
-        return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-    },
-
-    // Apparently, thefollwoing line is not required make pinning work on touch screen (since 4.0.0 ??)
-    // but (since 4.0.0 ??) the grabbing seems buggy, so 3.6.1 is still being used in this demo 
-
-    pinType: document.querySelector('[data-scroll-container]').style.transform ? "transform" : "fixed"
-
-});
-
-ScrollTrigger.defaults({ scroller: '[data-scroll-container]' });
-
-
-
-
-gsap.set('section.footer-container', { yPercent: -50 })
-
-const uncover = gsap.timeline({ paused: true })
-
-uncover
-    .to('section.footer-container', { yPercent: 0, ease: 'none' })
-    ;
-
-ScrollTrigger.create({
-    trigger: 'section.conclusion',
-    start: 'bottom bottom',
-    end: '+=75%',
-    animation: uncover,
-    scrub: true,
-    markers: true,
-})
-
-
-
-
-
-ScrollTrigger.addEventListener("refresh", () => scroll.update());
-
-ScrollTrigger.refresh();  */
 
 const locoScroll = new LocomotiveScroll({
-    el: document.querySelector(".scroller"),
+    el: document.querySelector(".scroll-wrapper"),
     smooth: true,
 
     // // --- // for versions (since 4.0.0 ??)
@@ -70,7 +18,7 @@ const locoScroll = new LocomotiveScroll({
 });
 locoScroll.on("scroll", ScrollTrigger.update);
 
-ScrollTrigger.scrollerProxy(".scroller", {
+ScrollTrigger.scrollerProxy(".scroll-wrapper", {
     scrollTop(value) {
         return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
     },
@@ -81,26 +29,26 @@ ScrollTrigger.scrollerProxy(".scroller", {
     // Apparently, thefollwoing line is not required make pinning work on touch screen (since 4.0.0 ??)
     // but (since 4.0.0 ??) the grabbing seems buggy, so 3.6.1 is still being used in this demo 
 
-    pinType: document.querySelector(".scroller").style.transform ? "transform" : "fixed"
+    pinType: document.querySelector(".scroll-wrapper").style.transform ? "transform" : "fixed"
 
 });
 
-ScrollTrigger.defaults({ scroller: '.scroller' });
+ScrollTrigger.defaults({ scroller: '.scroll-wrapper' });
 
 
 
 
 
-gsap.set('section.footer-container', { yPercent: -50 })
+gsap.set('section.footer__container', { yPercent: -50 })
 
 const uncover = gsap.timeline({ paused: true })
 
 uncover
-    .to('section.footer-container', { yPercent: 0, ease: 'none' })
+    .to('section.footer__container', { yPercent: 0, ease: 'none' })
     ;
 
 ScrollTrigger.create({
-    trigger: 'section.hero',
+    trigger: '.scroll-wrapper__content',
     start: 'bottom bottom',
     end: '+=75%',
     animation: uncover,
