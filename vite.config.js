40b551feb48dc2487fa575eb9e024d27dddf0f49
Léo Genot <lg@mambomambo.ca>
2022-10-07 14:22:29 +0200

    working template

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import path from "path"
 
 
 export default defineConfig({
     resolve: { alias: { '@': '/src' } },
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        createSvgIconsPlugin({
+            iconDirs: [path.resolve(process.cwd(), "src/assets/svg")],
+            symbolId: "icon-[dir]-[name]",
+            customDomId: "spritesheet",
+        }),
+    ],
     css: {
         preprocessorOptions: {
             scss: {
