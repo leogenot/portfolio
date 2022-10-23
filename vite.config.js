2ed291e96f71134ca32ef2b76e9172dc41131b0a
Léo Genot <lg@mambomambo.ca>
2022-10-23 18:19:32 +0200

    settings

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import path from "path"
 
 
-export default defineConfig({
+/* export default defineConfig({
     resolve: { alias: { '@': '/src' } },
     plugins: [
         vue(),
@@ -24,4 +24,53 @@ export default defineConfig({
     },
     publicPath: process.env.NODE_ENV === "production" ? "/leogenot.github.io/" : "/",
 
-})
+}) */
+
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        base: command === "serve" ? "" : "/dist/",
+        build: {
+            manifest: true,
+            outDir: "../public_html/dist/",
+            rollupOptions: {
+                input: {
+                    app: "./src/main.js",
+                },
+            },
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@import "@assets/scss/main.scss";`,
+                },
+            },
+            devSourcemap: true,
+        },
+        envDir: "",
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+                "@assets": path.resolve(__dirname, "./src/assets"),
+                "@templates": path.resolve(__dirname, "./src/templates"),
+                "@layout": path.resolve(__dirname, "./src/templates/layout"),
+                "@components": path.resolve(__dirname, "./src/templates/components"),
+                "@objects": path.resolve(__dirname, "./src/templates/objects"),
+                "@views": path.resolve(__dirname, "./src/templates/views"),
+            },
+        },
+        plugins: [
+            vue(),
+            createSvgIconsPlugin({
+                iconDirs: [path.resolve(process.cwd(), "src/assets/svg")],
+                symbolId: "icon-[dir]-[name]",
+                customDomId: "spritesheet",
+            }),
+        ],
+        optimizeDeps: {
+            include: ["vue", "vue-router"],
+            exclude: [],
+        },
+    };
+});
